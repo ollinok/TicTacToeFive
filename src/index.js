2f8677ef656669b05fdcd45d8e812ee91f52515a
ollinok <62589655+ollinok@users.noreply.github.com>
2021-08-19 01:17:46 +0300

    winning conditions added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom';
 import Gameboard from './Gameboard.js';
 import './styles/index.css';
 
-const boardHeight = 20;
-const boardWidth = 20;
+const boardHeight = 10;
+const boardWidth = 10;
 class Game extends React.Component {
     // constructor(props) {
     //     super(props);
@@ -20,10 +20,63 @@ class Game extends React.Component {
     //     squares[i] = 'X';
     //     this.setState({squares: squares});
     // }
+    winCondition() {
+        const he = boardHeight;
+        const wi = boardWidth;
+        const winLines = [];
+        // horizontal
+        for (let i=0;i<he;i++) {
+            for (let u=0;u<(wi-4);u++) {
+                winLines.push(
+                    [u+(wi*i),
+                     u+(wi*i)+1,
+                     u+(wi*i)+2,
+                     u+(wi*i)+3,
+                     u+(wi*i)+4
+                    ]);
+            }
+        }
+        // vertical
+        for (let i=0;i<wi;i++) {
+            for (let u=0;u<(he-4);u++) {
+                winLines.push(
+                    [i+(wi*u),
+                     i+(wi*u)+wi,
+                     i+(wi*u)+wi*2,
+                     i+(wi*u)+wi*3,
+                     i+(wi*u)+wi*4
+                    ]);
+            }
+        }
+        // diagonal
+        for (let i=0;i<(he-4);i++) {
+            for (let u=0;u<(wi-4);u++) {
+                winLines.push(
+                    [u+(wi*i),
+                     u+(wi*i)+1+wi,
+                     u+(wi*i)+2+wi*2,
+                     u+(wi*i)+3+wi*3,
+                     u+(wi*i)+4+wi*4
+                    ]);
+            }
+            for (let u=0;u<(wi-4);u++) {
+                winLines.push(
+                    [u+4+(wi*i),
+                     u+3+(wi*i)+wi,
+                     u+2+(wi*i)+wi*2,
+                     u+1+(wi*i)+wi*3,
+                     u+(wi*i)+wi*4,
+                    ]);
+            }
+        }
+        return winLines;
+    }
 
     render() {
+        const testi = this.winCondition();
         return (
             <div className="game">
+                <p>Length: {testi.length} | {testi[35]}</p>
               <div className="game-board">
                 <Gameboard boardHeight={boardHeight}
                            boardWidth={boardWidth}
